cfba8e38b016b14547adc0af9f0af7cdfcb23f81
fahad <fahad@10.131.219.45>
2016-12-28 13:54:46 +0300

    Add My Name Fahad

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -8,6 +8,7 @@ public class firstclass {
 		System.out.println("please add two numbers");
 		System.out.println("Result is: "+ add(x,y));
 		//add(x,y);
+		String F =  "FahadBaderAlmtereu";
 
 	}
 	
